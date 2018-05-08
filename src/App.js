d99b63b0e9a93657b909e5abade8e313f12c1d75
John Higgins <jhiggins0@gmail.com>
2018-05-08 12:19:10 -0700

    completed basic react-redux TodoList example

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Footer from "./components/Footer";
+import AddTodo from "./containers/AddTodo";
+import VisibleTodoList from "./containers/VisibleTodoList";
 
 class App extends Component {
   render() {
@@ -8,11 +11,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to #HigTodo</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AddTodo />
+        <VisibleTodoList />
+        <Footer />
       </div>
     );
   }
---
+++ src/containers/VisibleTodoList.js
@@ -0,0 +1,31 @@
+import { connect } from "react-redux";
+import { toggleTodo } from "../actions";
+import TodoList from "../components/TodoList";
+
+const getVisibleTodos = (todos, filter) => {
+  switch (filter) {
+    case "SHOW_COMPLETED":
+      return todos.filter(todo => todo.completed);
+
+    case "SHOW_ACTIVE":
+      return todos.filter(todo => !todo.completed);
+
+    case "SHOW_ALL":
+      return todos;
+
+    default:
+      throw new Error("Unknown filter: " + filter);
+  }
+};
+
+const mapStateToProps = state => {
+  return { todos: getVisibleTodos(state.todos, state.visibilityFilter) };
+};
+
+const mapDispatchToProps = dispatch => {
+  return { onTodoClick: id => dispatch(toggleTodo(id)) };
+};
+
+const VisibleTodoList = connect(mapStateToProps, mapDispatchToProps)(TodoList);
+
+export default VisibleTodoList;
